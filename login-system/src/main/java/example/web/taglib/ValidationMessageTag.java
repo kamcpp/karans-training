71f0ad34da9ad8d3ce57f850bc57895f834fef58
Kamran Amini <kamcpp@gmail.com>
2014-09-09 13:44:01 +0430

    Taglib dependency errors were corrected.

--- login-system/src/main/java/example/web/taglib/ValidationMessageTag.java
+++ login-system/src/main/java/example/web/taglib/ValidationMessageTag.java
@@ -1,5 +1,39 @@
 package example.web.taglib;
 
-public class ValidationMessageTag {
+import java.io.IOException;
+import java.util.Set;
 
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+import javax.validation.ConstraintViolation;
+
+import org.apache.log4j.Logger;
+
+public class ValidationMessageTag extends SimpleTagSupport {
+
+	Logger logger = Logger.getLogger(ValidationMessageTag.class);
+
+	private String field;
+
+	public void setField(String field) {
+		this.field = field;
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public void doTag() throws JspException, IOException {
+		Set<ConstraintViolation<Object>> violations = (Set<ConstraintViolation<Object>>) getJspContext()
+				.getAttribute("violations");
+		for (ConstraintViolation<Object> violation : violations) {
+			logger.trace(violation.getPropertyPath());
+			if (violation.getPropertyPath().equals(field)) {
+				JspWriter out = getJspContext().getOut();
+				out.write("<span style='color: red'>" + violation.getMessage()
+						+ "</span>");
+				out.flush();
+				break;
+			}
+		}
+	}
 }
